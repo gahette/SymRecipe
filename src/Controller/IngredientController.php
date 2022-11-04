167835e01108dd8d058acc572a2f4eb5370e1223
gahette <gahette@icloud.com>
2022-11-04 15:02:26 +0100

    📖 read add pagination - CRUD

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,20 +3,36 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ */
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'ingredient')]
-    public function index(IngredientRepository $repository): Response
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient',methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
 
-        $ingredients = $repository->findAll();
+        $ingredient = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), 10
+        );
 
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $ingredients
+            'ingredients' => $ingredient
         ]);
     }
 }
