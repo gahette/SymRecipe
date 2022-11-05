23aef96f3ce44b2878d7ab48e5e25af7daf88833
gahette <gahette@icloud.com>
2022-11-05 03:29:19 +0100

    🦺 create update - CRUD

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -39,6 +39,13 @@ class IngredientController extends AbstractController
         ]);
     }
 
+    /**
+     * This controller show a form create an ingredient
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
     #[Route('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $manager): Response
     {
@@ -58,10 +65,35 @@ class IngredientController extends AbstractController
             );
 
             return $this->redirectToRoute('ingredient.index');
-
         }
 
         return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods: ['GET', 'POST'])]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été modifié avec succes !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+
+        return $this->render('pages/ingredient/edit.html.twig', [
             'form' => $form->createView()
         ]);
     }
